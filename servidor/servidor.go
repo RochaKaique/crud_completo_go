943e48ce122387af5d345c10cc4e5286cb073644
Kaique Rocha <kaiquerocha@MacBook-Air-de-Kaique.local>
2024-05-11 12:25:27 -0300

    :tada: CRUD completo :tada:

--- servidor/servidor.go
+++ servidor/servidor.go
@@ -3,6 +3,7 @@ package servidor
 import (
 	"crud/banco"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -121,4 +122,77 @@ func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+}
+
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	ID := parametros["id"]
+
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Erro ao ler corpo da requisição"))
+		return
+	}
+
+	var usuario usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao converter usuário para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("update usuario set nome = ?, email = ? where id = ?")
+	if erro != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao criar statement"))
+		fmt.Println(erro)
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuario.Nome, usuario.Email, ID); erro != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao executar statement"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	ID := parametros["id"]
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("delete from usuario where id = ?")
+	if erro != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao criar statement"))
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(ID); erro != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao executar statement"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
